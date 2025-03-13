1cf5420f1c5ef56ae8e32835f80206af5b46c173
Egor Babilo <egorBabiloLinux@yandex.com>
2025-03-13 20:56:58 +0300

    add test

--- tests/url_shortener_test.go
+++ tests/url_shortener_test.go
@@ -8,11 +8,16 @@ import (
 
 	"github.com/brianvoe/gofakeit/v6"
 	"github.com/gavv/httpexpect/v2"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 const (
 	host = "localhost:8082"
+	passDefaultLen = 10
 )
+
+const app_id = 1
 
 func TestURLShortener_HappyPath(t *testing.T) {
 	u := url.URL{
@@ -22,20 +27,42 @@ func TestURLShortener_HappyPath(t *testing.T) {
 
 	e := httpexpect.Default(t, u.String())
 
-	username := gofakeit.Email()
+	email := gofakeit.Email()
 	password := randomFakePassword()
 
 	response := e.POST("/register").WithJSON(map[string]string{
-		"username": username,
+		"email": email,
 		"password": password,
-	})
+	}).Expect().Status(200)
+
+	respJson := response.JSON().Object()
+
+	status := respJson.Value("status").String().Raw()
+	assert.Equal(t, status, "OK")
+	
+	uid := respJson.Value("uid").Number().Raw()
+	require.NotEmpty(t, uid)
+	
+	response = e.POST("/login").WithJSON(map[string]interface{}{
+		"email": email,
+		"password": password,
+		"app_id": app_id,
+	}).Expect().Status(200)
+
+	respJson = response.JSON().Object()
+
+	token := respJson.Value("token").String().Raw()
+	assert.NotEmpty(t, token)
 
 	e.POST("/url").WithJSON(save.Request{
 		URL: gofakeit.URL(),
 		Alias: random.NewRandomString(10),
 	}).
-	
-	
+	WithHeader("Authorization", "Bearer " + token).
+	Expect().
+	Status(200).
+	JSON().Object().
+	ContainsKey("alias")
 }
 
 func randomFakePassword() string {
